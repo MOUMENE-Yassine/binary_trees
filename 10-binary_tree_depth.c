7e0ef66f2c1ab71206ad409458a610f7e473888b
SanaeMN <Sanaeenemuom2017@gmail.com>
2023-11-02 13:02:28 -0700

    10. Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - function that measures the depth of a node in a binary tree
+ * binary_tree_depth - function that measures the depth of a node
  * @node: binary_tree_t node
  * Return: 0.
  */
